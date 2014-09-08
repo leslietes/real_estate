97390edeaf687a7ff00261b8d9e09fbf0561c4df
Leslie T <leslie_tanengsy@yahoo.com>
2014-09-08 14:34:25 +0800

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealEstate::Application.config.secret_token = '58c9a5a6b8198610c4b8a559c7860a1699fcaf7e223cf9b47c528395f350d4e8bd11a6fc33d6ca67bdf6be11135b5503057505c2a8027e80cded847824389d3e'
